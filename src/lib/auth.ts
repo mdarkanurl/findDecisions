80bb5e2cad66180d9f7a7efc49ec41c815b69069
Mohammad Arkan <mdarkanurl@gmail.com>
2025-12-23 11:27:30 +0600

    Better-auth setup done

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,13 +1,14 @@
 import { betterAuth } from 'better-auth';
 import { prismaAdapter } from 'better-auth/adapters/prisma';
-import { PrismaService } from '../prisma/prisma.service';
+import { PrismaService } from "../prisma/prisma.service";
+import 'dotenv/config';
 
-const prisma = new PrismaService();
+const Prisma = new PrismaService();
 
 export const auth = betterAuth({
   url: process.env.BETTER_AUTH_URL!,
   secret: process.env.BETTER_AUTH_SECRET!,
-  database: prismaAdapter(prisma, {
+  database: prismaAdapter(Prisma, {
     provider: 'postgresql',
   }),
   emailAndPassword: {
--- src/prisma/prisma.service.ts
+++ src/prisma/prisma.service.ts
@@ -1,12 +1,20 @@
 import { Injectable, OnModuleInit } from '@nestjs/common';
 import { PrismaClient } from '../../generated/prisma/client';
 import { PrismaPg } from '@prisma/adapter-pg';
+import { Pool } from 'pg';
+import 'dotenv/config';
 
 @Injectable()
 export class PrismaService extends PrismaClient implements OnModuleInit {
   constructor() {
-    const pool = new PrismaPg({ connectionString: process.env.DATABASE_URL! });
-    super({ adapter: pool });
+    const url = process.env.DATABASE_URL;
+    
+    if (!url) {
+      throw new Error("DATABASE_URL is not defined in environment variables");
+    }
+    const pool = new Pool({ connectionString: url });
+    const adapter = new PrismaPg(pool);
+    super({ adapter });
   }
 
   async onModuleInit() {
