4d107c7adcc0ef8a71e74b766367efcfaae3861e
Mohammad Arkan <mdarkanurl@gmail.com>
2026-01-06 21:16:28 +0600

    Added max and min length of password

--- src/auth/dto/create.signUpEmail.dto.ts
+++ src/auth/dto/create.signUpEmail.dto.ts
@@ -3,7 +3,7 @@ import { z } from 'zod';
 export const createUserSchema = z.object({
     name: z.string(),
     email: z.string(),
-    password: z.string(),
+    password: z.string().min(8).max(128),
     image: z.string().optional(),
     callbackURL: z.string().optional()
 });
