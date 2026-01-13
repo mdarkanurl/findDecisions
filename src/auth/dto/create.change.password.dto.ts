bb79cdf6818b4df6458e84765cee66cf0b3a5d54
Mohammad Arkan <mdarkanurl@gmail.com>
2026-01-13 19:14:28 +0600

    Fixed dto

--- src/auth/dto/create.change.password.dto.ts
+++ src/auth/dto/create.change.password.dto.ts
@@ -1,8 +1,8 @@
 import { z } from 'zod';
 
 export const changePasswordSchema = z.object({
-    email: z.string().email(),
-    password: z.string().min(8).max(128)
+    currentPassword: z.string().min(8).max(128),
+    newPassword: z.string().min(8).max(128)
 });
 
 export type changePasswordSchemaDto = z.infer<typeof changePasswordSchema>;
