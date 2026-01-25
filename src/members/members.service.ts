5390f5f63ba06a0e1d6e44ac31c7ac9e5fec9732
Mohammad Arkan <mdarkanurl@gmail.com>
2026-01-25 19:22:19 +0600

    Removed files

--- src/members/members.service.ts
+++
@@ -1,8 +0,0 @@
-import { Injectable, NotFoundException } from '@nestjs/common';
-import { PrismaService } from './../prisma/prisma.service';
-
-@Injectable()
-export class membersService {
-  constructor (private prisma: PrismaService) {};
-  
-}
